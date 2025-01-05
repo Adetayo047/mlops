63ccd562a44fc012c6584973204d71fd2cc1ad9c
Adetunji Philip <44329526+Adetayo047@users.noreply.github.com>
2025-01-06 00:39:24 +0100

    Create train.py

---
+++ src/train.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from joblib import dump
+
+def train_model():
+    # Dummy dataset
+    data = pd.DataFrame({
+        'feature1': [1, 2, 3, 4, 5],
+        'feature2': [2, 3, 4, 5, 6],
+        'target': [3, 5, 7, 9, 11]
+    })
+
+    # Train-test split
+    X = data[['feature1', 'feature2']]
+    y = data['target']
+
+    # Train model
+    model = LinearRegression()
+    model.fit(X, y)
+
+    # Save the model
+    dump(model, '../models/linear_model.joblib')
+    print("Model trained and saved!")
+
+if __name__ == "__main__":
+    train_model()
